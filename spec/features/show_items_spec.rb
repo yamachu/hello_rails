71f41a94c34973e526e71c3f22bd00447899ee00
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-08-13 03:21:32 +0900

    add recommended test, it fail

--- spec/features/show_items_spec.rb
+++ spec/features/show_items_spec.rb
@@ -10,4 +10,15 @@ RSpec.feature "See shop items", type: :feature do
 		expect(page).to have_text('包丁')
 		expect(page).to have_text('フライパン')
 	end
+
+	scenario 'A user can see recommended items' do
+		create(:item, name: '包丁', recommended: true)
+		create(:item, name: 'フライパン')
+
+		visit recommended_items_path
+
+		expect(page).to have_text('包丁')
+		expect(page).not_to have_text('フライパン')
+	end
+
 end
