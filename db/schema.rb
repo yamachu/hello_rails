8141d37370539514b0d227c91bea57e9163b400b
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-08-13 03:36:27 +0900

    add category mock

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812182209) do
+ActiveRecord::Schema.define(version: 20160812183331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "null"
+    t.string   "false"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
