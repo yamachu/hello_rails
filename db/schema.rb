238f77a5acb3b5167d9e4a43c8915e7b615809a9
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-08-13 03:23:41 +0900

    add recommended db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812180418) do
+ActiveRecord::Schema.define(version: 20160812182209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20160812180418) do
     t.string   "description"
     t.integer  "price"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.boolean  "recommended", default: false, null: false
   end
 
 end
