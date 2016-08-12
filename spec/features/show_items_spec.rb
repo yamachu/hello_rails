8a242edd430fdc4d60af8040d2d1640fdc91ebb8
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-08-13 04:04:58 +0900

    user access test, it pending

--- spec/features/show_items_spec.rb
+++ spec/features/show_items_spec.rb
@@ -21,4 +21,21 @@ RSpec.feature "See shop items", type: :feature do
 		expect(page).not_to have_text('フライパン')
 	end
 
+	xscenario 'A user can see categorized items from portal', type: :feature do
+		kitchenware = create(:category, name: '調理器具')
+		kitchenware.items.create!(name: '包丁', recommended: true)
+		kitchenware.items.create!(name: 'フライパン')
+
+		tableware = create(:category, name: '食器')
+		tableware.items.create!(name: '大皿')
+
+		visit root_path
+		click_on '調理器具'
+
+		expect(page).to have_text('包丁')
+		expect(page).to have_text('フライパン')
+
+		expect(page).not_to have_text('大皿')
+	end
+
 end
