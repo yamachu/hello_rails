f2624682cf368846df3e8ad3b6714fa124f3d990
Yusuke Yamada <yusuke.yamada.t1@dc.tohoku.ac.jp>
2016-08-13 04:18:29 +0900

    add nested categorized items

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   		get :recommended
   	end
   end
+
+  resources :categories, only: %i(index) do
+  	resources :items, only: %w(index)
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812183331) do
+ActiveRecord::Schema.define(version: 20160812190954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20160812183331) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "recommended", default: false, null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_items_on_category_id", using: :btree
   end
 
 end
